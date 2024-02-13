bd87a0df4366e5d7e18f5d616cbb38c4cbb30d39
shako metonidze <120588118+shakomentol@users.noreply.github.com>
2024-02-13 13:27:51 -0800

    i just created header and footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponentComponent, FooterComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Dental-project';
